ce9332c39df7b87a6b561a4a30904f7aa43908b3
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-28 18:44:19 +0200

    Installer erweitert

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
+define('DRIPS_STARTUP', __DIR__.'/vendor/drips/drips/index.php');
+
 if(!is_dir(__DIR__.'/vendor')){
     echo '<h1>Welcome to Drips!</h1>';
     echo '<p>Please execute <code>php install</code> on the command line to continue!</p>';
+} else {
+    if(!defined('DRIPS_DIRECTORY')){
+        define('DRIPS_DIRECTORY', __DIR__);
+    }
+    if(!include(DRIPS_STARTUP)){
+        die('Die Drips-Installation ist fehlgeschlagen!');
+    }
 }
